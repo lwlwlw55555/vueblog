49705cc50646b44aa900380a5895766cb481ecf4
lw <lw>
2022-06-14 18:12:11 +0800

    add base

--- src/router/index.js
+++ src/router/index.js
@@ -48,8 +48,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  // base: '/web/',
-  base: process.env.BASE_URL,
+  base: '/web/',
+  // base: process.env.BASE_URL,
   routes
 })
 
