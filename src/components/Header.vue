5555e10333353dfd5ea52548d72ac6f6f9f8151b
lw <lw>
2022-06-15 16:47:23 +0800

    add /web/

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
     </div>
 
     <div class="maction">
-      <span><el-link href="/blogs">主页</el-link></span>
+      <span><el-link href="/web/blogs">主页</el-link></span>
       <el-divider direction="vertical"></el-divider>
       <span><el-link type="success" href="/web/blog/add">发表博客</el-link></span>
 
